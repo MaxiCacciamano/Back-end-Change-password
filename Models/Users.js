d3171e2bbdfcc4a0cb10a35bc0cc8e896115266d
MaxiCacciamano <maximilianocacciamano@gmail.com>
2025-05-20 14:36:58 -0300

    ko

--- Models/Users.js
+++ Models/Users.js
@@ -4,9 +4,7 @@ const wifiConfigSchema = new mongoose.Schema({
     nombreCuenta:{type:String, required:true},
     ssid:{type:String, required:true},
     password:{type:String,required:true},
-    // updateAt:{type:Date, default:Date.now}
-},{
-    timestamps:true
+    updateAt:{type:Date, default:Date.now}
 });
 
 module.exports = mongoose.model('WifiConfig', wifiConfigSchema)
--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors({
-  origin:'http://localhost:3000', //Front
+  origin:'*', //Front
   credentials: true //si se utilizan cookies o auth headers
 }))
 
@@ -21,4 +21,4 @@ app.use('/api/auth', authRoutes)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(process.env.PORT, ()=>console.log(`🚀Servidor corriendo en el puerto ${PORT}`))
+app.listen(process.env.PORT,'0.0.0.0', ()=>console.log(`🚀Servidor corriendo en el puerto ${PORT}`))
