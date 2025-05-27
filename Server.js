d65ff5e9d261c8eb2bb0045aac7bffd88d99ab32
MaxiCacciamano <maximilianocacciamano@gmail.com>
2025-05-27 12:30:11 -0300

    api

--- Server.js
+++ Server.js
@@ -6,11 +6,23 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cors = require('cors')
 
+const allowedOrigins = [
+  process.env.FRONT,
+  process.env.DOMINIO,
+  process.env.PUERTA_DESARROLLO
+]
+
 const app = express()
 
 app.use(express.json());
 app.use(cors({
-  origin:'http://localhost:3000', //Front
+  origin: function (origin, callback) {
+    if(allowedOrigins.includes(origin)){
+      return callback(null, true);
+    }else{
+      return callback(new Error('CORS: Origen no permitido'), false)
+    }
+  },
   credentials: true //si se utilizan cookies o auth headers
 }))
 
@@ -18,7 +30,11 @@ app.use(session({
   secret:process.env.JWT_SECRET,
   resave:false,
   saveUninitialized:false,
-  cookie:{secure:false} //secure: true solo con HTTPS
+  cookie:{
+    secure:true,
+    httpOnly: true,
+    sameSite: 'lax'
+  } //secure: true solo con HTTPS
 }))
 
 
@@ -30,4 +46,6 @@ app.use('/api/auth', authRoutes)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(process.env.PORT,'0.0.0.0', ()=>console.log(`🚀Servidor corriendo en el puerto ${PORT}`))
+app.listen(process.env.PORT,'0.0.0.0', ()=>
+  console.log(`🚀Servidor corriendo en el puerto ${PORT}`)
+)
